29346163fe86bc8627ab5fa6ca065b4b3c0821e1
Neil Empey <argus.two.2@gmail.com>
2022-01-10 21:13:46 -0700

    procfile 7

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-       origins ['localhost:3000', 'https://neils-tripcast.herokuapp.com/']
+       origins ['http://127.0.0.1:3001/destinations', 'https://neils-tripcast.herokuapp.com/']
 
     resource '*',
       headers: :any,

1e96a8732dfc6f2a9aa6dc9af5cf08b3ff2083b6
Neil Empey <argus.two.2@gmail.com>
2022-01-16 10:25:54 -0700

    heroku config1

--- client/src/components/Inputs.js
+++ client/src/components/Inputs.js
@@ -55,7 +55,7 @@ class Inputs extends React.Component {
         let data = response.data;
         this.setState({ route: data.routeDirections });
         this.setState({ weather: data.routeWeather });
-        this.setState({ url: (`https://www.mapquestapi.com/staticmap/v5/map?start=${this.state.origin}|flag-start&end=${this.state.dest}|flag-end&size=@2x&key=` + process.env.consumer_key).toString()});
+        this.setState({ url: ((`https://www.mapquestapi.com/staticmap/v5/map?start=${this.state.origin}|flag-start&end=${this.state.dest}|flag-end&size=@2x&key=`).toString() + (`${process.env.consumer_key}`).toString())});
       });
 
     this.setState({ origin: "", dest: "", isActive: false });

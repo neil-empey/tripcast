f57341061b4e26a5a2b3c721ffd49abef66cb267
Neil Empey <argus.two.2@gmail.com>
2022-01-15 18:51:59 -0700

    cloudinary.rb1

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,8 +1,8 @@
 
 Cloudinary.config do |config|
-config.cloud_name: ENV.fetch("cloud_name")
-config.api_key: ENV.fetch("api_key")
-config.api_secret: ENV.fetch("api_secret")
+config.cloud_name: #{ENV.fetch(cloud_name)}
+config.api_key: #{ENV.fetch(api_key)}
+config.api_secret: #{ENV.fetch(api_secret)}
 config.secure: true
 config.cdn_subdomain: true
 end

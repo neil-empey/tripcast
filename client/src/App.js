e698adb95524532ed796baea7f38ec099a16686c
Neil Empey <argus.two.2@gmail.com>
2022-01-18 19:05:44 -0700

    css14

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
           <div className="spacer"></div>
           <h2 className="subhead">Directions and Weather</h2>
           <Inputs />
-        </div>
+
         <div className="spacer"></div>
         <div className="gif">
           <iframe

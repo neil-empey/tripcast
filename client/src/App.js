9d9e099fce25b2613e97bd61d5dd37f5f1db1105
Neil Empey <argus.two.2@gmail.com>
2022-01-18 19:08:49 -0700

    css15

--- client/src/App.js
+++ client/src/App.js
@@ -13,12 +13,13 @@ function App() {
           <h2 className="subhead">Directions and Weather</h2>
           <Inputs />
 
-        <div className="spacer"></div>
-        <div className="gif">
-          <iframe
-            id="author-avatar"
-            src="https://giphy.com/embed/LBgf56psa07tCUJmpC"
-          ></iframe>
+          <div className="spacer"></div>
+          <div className="gif">
+            <iframe
+              id="author-avatar"
+              src="https://giphy.com/embed/LBgf56psa07tCUJmpC"
+            ></iframe>
+          </div>
         </section>
       </div>
     </div>

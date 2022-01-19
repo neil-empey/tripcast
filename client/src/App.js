0904ffe55e67acd8ee2b3707121ed3315cfa884f
Neil Empey <argus.two.2@gmail.com>
2022-01-19 00:07:46 -0700

    css app3

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,10 @@ import React, { useState } from "react";
 function App() {
   return (
     <div className="app">
-      <div className="">
-        <div className="">
-          <h1 className="trip-cast">TripCast</h1>
-          <h5 className="trip">Directions and Weather</h5>
-          <p>Always Prepared</p>
-          <Inputs />
-        </div>
-      </div>
+      <h1 className="trip-cast">TripCast</h1>
+      <h5 className="trip">Directions and Weather</h5>
+      <p>Always Prepared</p>
+      <Inputs />
     </div>
   );
 }

fdcd3aa2ecbfbc1cf8180787414e068a7f9886b7
Neil Empey <argus.two.2@gmail.com>
2022-01-19 00:01:06 -0700

    css app1

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="app">
       <div className="">
-        <div className="app">
+        <div className="">
           <h1 className="trip-cast">TripCast</h1>
           <h5 className="trip">Directions and Weather</h5>
           <p>Always Prepared</p>

ad120f67633679592b30c188451a5d7cb7814d31
Neil Empey <argus.two.2@gmail.com>
2022-01-18 19:03:08 -0700

    css13

--- client/src/App.js
+++ client/src/App.js
@@ -7,17 +7,19 @@ function App() {
   return (
     <div>
       <div id="app">
-        <h1 className="title">TripCast</h1>
+        <section>
+          <h1 className="title">TripCast</h1>
+          <div className="spacer"></div>
+          <h2 className="subhead">Directions and Weather</h2>
+          <Inputs />
+        </div>
         <div className="spacer"></div>
-        <h2 className="subhead">Directions and Weather</h2>
-        <Inputs />
-      </div>
-      <div className="spacer"></div>
-      <div className="gif">
-        <iframe
-          id="author-avatar"
-          src="https://giphy.com/embed/LBgf56psa07tCUJmpC"
-        ></iframe>
+        <div className="gif">
+          <iframe
+            id="author-avatar"
+            src="https://giphy.com/embed/LBgf56psa07tCUJmpC"
+          ></iframe>
+        </section>
       </div>
     </div>
   );
